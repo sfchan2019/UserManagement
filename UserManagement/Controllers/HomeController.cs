34e7258a6ce8d64ba8555e3b1203ad12bf69d9ea
Xaritomi <44918432+XaritomiX@users.noreply.github.com>
2019-09-23 15:47:11 +0100

    Added Test DB

--- UserManagement/Controllers/HomeController.cs
+++ UserManagement/Controllers/HomeController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using UserManagement.Models;
 
 namespace UserManagement.Controllers
 {
     public class HomeController : Controller
     {
+        UserDatabaseEntities db = new UserDatabaseEntities();
+        
         public ActionResult Login()
         {
             ViewBag.Message = "Login";
-
+           
             return View();
         }
 
---
+++ UserManagement/Controllers/TestControlle.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using UserManagement.Models;
+
+namespace UserManagement.Controllers
+{
+    public class TestControlle : Controller
+    {
+        private UserDatabaseEntities db = new UserDatabaseEntities();
+
+        // GET: TestControlle
+        public ActionResult Index()
+        {
+            return View(db.Logins.ToList());
+        }
+
+        // GET: TestControlle/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Login login = db.Logins.Find(id);
+            if (login == null)
+            {
+                return HttpNotFound();
+            }
+            return View(login);
+        }
+
+        // GET: TestControlle/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TestControlle/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,username,password,admin")] Login login)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Logins.Add(login);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(login);
+        }
+
+        // GET: TestControlle/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Login login = db.Logins.Find(id);
+            if (login == null)
+            {
+                return HttpNotFound();
+            }
+            return View(login);
+        }
+
+        // POST: TestControlle/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,username,password,admin")] Login login)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(login).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(login);
+        }
+
+        // GET: TestControlle/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Login login = db.Logins.Find(id);
+            if (login == null)
+            {
+                return HttpNotFound();
+            }
+            return View(login);
+        }
+
+        // POST: TestControlle/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Login login = db.Logins.Find(id);
+            db.Logins.Remove(login);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
