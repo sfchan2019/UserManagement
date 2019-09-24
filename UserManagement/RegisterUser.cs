9291be3f21f8bcb472d28061c9df520f9c72c7cf
BrandonStephen <54633532+BrandonStephen@users.noreply.github.com>
2019-09-24 15:24:20 +0100

    POST Method created

---
+++ UserManagement/RegisterUser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using UserManagement.Models;
+
+namespace UserManagement
+{
+    public class RegisterUser
+    {
+        public static string post(Login register)
+        {
+            string success;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:44317/api");
+                var postUserResult = client.PostAsXmlAsync("user", register);
+                postUserResult.Wait();
+                var result = postUserResult.Result;
+
+                if (result.IsSuccessStatusCode)
+                {
+                    success = "true";
+                }
+                else {
+                    success = "false";
+                }
+
+            }
+            return success;
+        }
+        
+    }
+}
